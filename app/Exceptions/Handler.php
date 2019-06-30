252ca440fc80a74a144ecb4a75a8bdbadfe5ea7e
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-06-30 10:29:13 -0500

    Validation for NotFoundHttpException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use App\Traits\ApiResponser;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -69,6 +70,10 @@ class Handler extends ExceptionHandler
 
         if($exception instanceof AuthorizationException){
             return $this->errorResponse("No posee permisos para ejecutar esta acción", 403);
+        }
+
+        if($exception instanceof NotFoundHttpException){
+            return $this->errorResponse("No se encontró la URL especificada", 404);
         }
 
         return parent::render($request, $exception);
