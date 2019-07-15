ec1d7603df4177da5cf8a0ee91ec769065378f0a
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2019-07-14 19:06:10 -0500

    Actualización de la migracion de usuarios para incluir campos necesarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,26 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('website')->nullable();
             $table->string('password');
+            $table->boolean('send_notifications')->default(true);
+            $table->integer('role_id');
+            $table->string('url_image')->nullable();
+            $table->string('address_1');
+            $table->string('address_2')->nullable();
+            $table->string('city');
+            $table->string('state')->nullable();
+            $table->string('zip');
+            $table->string('country');
+            $table->string('day_phone');
+            $table->boolean('tos')->default(false);
+            $table->string('company');
+            $table->boolean('enable')->default(false);
             $table->rememberToken();
             $table->string('verified')->default(User::USUARIO_NO_VERIFICADO);
             $table->string('verification_token')->nullable();
