ebfb63ed24c2b9efaa1d986a3bacc4dd35c419ce
Omar Alejandro Cervantes Yepez <omar@aumenta.mx>
2019-08-09 12:00:31 -0500

    Se agregó estructura de downloads al proyecto

---
+++ database/migrations/2019_08_09_162227_create_package_new_version_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackageNewVersionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('package_new_version', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('version_code')->unique();
+            $table->string('version_name');
+            $table->boolean('current_version')->default(false);
+            $table->timestamp('release_date');
+            $table->integer('estimate_size');
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('package_new_version');
+    }
+}
