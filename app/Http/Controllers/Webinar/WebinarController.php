659c729d953effd9cb06ce73da4303f6abddf40e
Omar Alejandro Cervantes Yepez <omar@aumenta.mx>
2019-08-20 12:36:14 -0500

    Creación de archivos para estructura de los LiveWebinars

---
+++ app/Http/Controllers/Webinar/WebinarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Webinar;
+
+use App\Http\Controllers\ApiController;
+use App\Webinar;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class WebinarController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Webinar $webinar
+     * @return Response
+     */
+    public function show(Webinar $webinar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Webinar $webinar
+     * @return Response
+     */
+    public function update(Request $request, Webinar $webinar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Webinar $webinar
+     * @return Response
+     */
+    public function destroy(Webinar $webinar)
+    {
+        //
+    }
+}
