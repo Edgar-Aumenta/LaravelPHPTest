bc2cac95706da57b281324b0558e9e1f4e47603c
Omar Alejandro Cervantes Yepez <omaralejandrocy@gmail.com>
2020-01-29 23:04:35 -0600

    Se agregó el metodo get para hacer peticiones hacia get guru

---
+++ app/GetGuruConstants.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App;
+
+
+class GetGuruConstants
+{
+    const URL_API = "https://api.getguru.com/api/v1/";
+    const Username = "colette@pcsynergy.com";
+    const Password = "0e50d1db-00cf-4484-b8a7-72fd2f0d4a3e";
+    const AuthorizationKey = "colette@pcsynergy.com:0e50d1db-00cf-4484-b8a7-72fd2f0d4a3e";
+}
--- app/Http/Controllers/GetGuru/GetGuruController.php
+++ app/Http/Controllers/GetGuru/GetGuruController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\GetGuru;
 
+use App\GetGuruConstants;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Client;
 
 class GetGuruController extends ApiController
 {
@@ -14,7 +17,19 @@ class GetGuruController extends ApiController
 
     public function GetGetGuru(Request $request)
     {
-        return response()->json($request, 200);
+        $client = new Client();
+        try {
+            $result = $client->get(GetGuruConstants::URL_API . $request->ns_method . '?' . $request->ns_params,
+                [
+                    'auth' =>  [GetGuruConstants::Username, GetGuruConstants::Password]
+                ]);
+        } catch (ClientException $e){
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            return response()->json(json_decode($responseBodyAsString), $response->getStatusCode());
+        }
+
+        return response()->json(json_decode($result->getBody()->getContents()), $result->getStatusCode());
     }
 
     public function PostGetGuru(Request $request)
