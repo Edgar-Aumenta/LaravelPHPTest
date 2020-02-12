79ca49953a10f824ad5435e4342dc31c13032ef6
Omar Alejandro Cervantes Yepez <omar@aumenta.mx>
2020-02-12 16:38:49 -0600

    Se cambió el titulo edl correo request more information

--- resources/views/emails/request_more_info.blade.php
+++ resources/views/emails/request_more_info.blade.php
@@ -6,20 +6,20 @@
     <title>PostalMate Feature Request</title>
 </head>
 <body>
-<p>PostalMate Feature Request</p>
-<ul>
-    <li>Name: {{ $requestMoreInfo->name }}</li>
-    <li>Email address: {{ $requestMoreInfo->email }}</li>
-    <li>Phone number: {{ $requestMoreInfo->phoneNumber }}</li>
-    <li>Store name: {{ $requestMoreInfo->storeName }}</li>
-    <li>Address: {{ $requestMoreInfo->address }}</li>
-    <li>City: {{ $requestMoreInfo->city }}</li>
-    <li>State: {{ $requestMoreInfo->state }}</li>
-    <li>Zip code: {{ $requestMoreInfo->zipCode }}</li>
-    <li>Current software: {{ $requestMoreInfo->currentSoftware }}</li>
-    <li>Request PostalMate trial?: {{ $requestMoreInfo->requestPMTrial }}</li>
-    <li>Store status: {{ $requestMoreInfo->storeStatus }}</li>
-    <li>Comments: {{ $requestMoreInfo->comments }}</li>
-</ul>
+<p>PostalMate Request More Information</p>
+    <ul>
+        <li>Name: {{ $requestMoreInfo->name }}</li>
+        <li>Email address: {{ $requestMoreInfo->email }}</li>
+        <li>Phone number: {{ $requestMoreInfo->phoneNumber }}</li>
+        <li>Store name: {{ $requestMoreInfo->storeName }}</li>
+        <li>Address: {{ $requestMoreInfo->address }}</li>
+        <li>City: {{ $requestMoreInfo->city }}</li>
+        <li>State: {{ $requestMoreInfo->state }}</li>
+        <li>Zip code: {{ $requestMoreInfo->zipCode }}</li>
+        <li>Current software: {{ $requestMoreInfo->currentSoftware }}</li>
+        <li>Request PostalMate trial?: {{ $requestMoreInfo->requestPMTrial }}</li>
+        <li>Store status: {{ $requestMoreInfo->storeStatus }}</li>
+        <li>Comments: {{ $requestMoreInfo->comments }}</li>
+    </ul>
 </body>
 </html>
